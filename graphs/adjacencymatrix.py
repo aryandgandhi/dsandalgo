56e9aaf2cf8d61008718b1fc6c6f907500d0e69b
Aryan Gandhi <aryandgandhi@gmail.com>
2024-04-17 10:33:45 -0500

    degree and isconnected

--- graphs/adjacencymatrix.py
+++ graphs/adjacencymatrix.py
@@ -1,22 +1,43 @@
 
 #this would be for undirected, for directed would just edit the add_edge method to create an edge one way
-#what about weighted? can store the weights in the graph, so for each edge take in a weight and set it accordingly
 class Graph:
-    def __init__(self, vertices):
+    def __init__(self, vertices, directed):
         self.vertices = vertices
         self.graph = [[0 for _ in range(vertices)] for i in range(vertices)]
+        self.directed = directed
 
-    def add_edge(self, u, v):
-        self.graph[u][v] = 1
-        #can remove below for directed
-        self.graph[v][u] = 1
+    def add_edge(self, u, v, weight=1):
+        
+        if u >= len(self.graph) or v >= len(self.graph) or u < 0 or v < 0:
+            return "can't call that" 
+        self.graph[u][v] = weight
+        
+        if not self.directed: 
+            self.graph[v][u] = weight
 
-    #weighted and directed add_edge: 
-    # def add_edge(self, u, v, weight):
-    #     self.graph[u][v] = weight
+    def remove_edge(self, u, v):
+        if u >= len(self.graph) or v >= len(self.graph) or u < 0 or v < 0:
+            return "can't call that" 
+        self.graph[u][v] = 0
+        
+        if not self.directed:
+            self.graph[v][u] = 0
 
-    def 
+    def get_adjacent_vertices(v):
+        
+        
+    def degree(self, v):
+        return len(self.graph[v]) - self.graph[v].count(0)
+        
+    def is_connected(self, u, v):
+        return self.graph[u][v] != 0
 
+    def bfs(self, v):
+        pass
+    
+    def dfs(self, v):
+        pass
+    
     def print_graph(self):
         for i in self.graph:
             for j in i:
